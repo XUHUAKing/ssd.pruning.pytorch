7e7d25a1f042c020024c05fff46eb5594d8917a7
XUHUAKing <xhuangat@connect.ust.hk>
2018-06-07 18:56:10 +0800

    modified name for sh files

--- startcoco.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-su root -c 'mount -o remount,size=128G /dev/shm'
-cd /cephfs/person/xuhuahuang/ssd.pytorch-master && python3 train.py --dataset COCO --dataset_root '/cephfs/share/data/coco_xy/'
--- startvoc.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-su root -c 'mount -o remount,size=128G /dev/shm'
-cd /cephfs/person/xuhuahuang/ssd.pytorch-master && python3 train.py
