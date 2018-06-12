ae393cbe3b2401bb100392a997553b068d2eceaf
XUHUAKing <xhuangat@connect.ust.hk>
2018-06-12 19:48:47 +0800

    back up before working on eval codes for WEISHI and COCO

--- utils/eval_tools.py
+++ utils/eval_tools.py
@@ -149,10 +149,6 @@ def do_python_eval(output_dir='output', use_07=True):
     print('{:.3f}'.format(np.mean(aps)))
     print('~~~~~~~~')
     print('')
-    print('--------------------------------------------------------------')
-    print('Results computed with the **unofficial** Python eval code.')
-    print('Results should be very close to the official MATLAB eval code.')
-    print('--------------------------------------------------------------')
 
 
 def voc_ap(rec, prec, use_07_metric=True):
