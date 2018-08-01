c02bf13a8df6215953c96b48d23c8d91723c3872
XUHUAKing <xhuangat@connect.ust.hk>
2018-08-01 16:36:08 +0800

    add pretrained model for backbones.py

--- models/backbones.py
+++ models/backbones.py
@@ -44,14 +44,16 @@ vgg_base = {
 def resnet():
     model = resnet50()
     model.load_state_dict(torch.load('weights/resnet50-19c8e357.pth')) # load pretrained model for detection
-    return model.resnet_layers()
+    return model.resnet_layers() # exclude fc but keep weights
 
 # return a list of model features for mobileNet v1
 def mobilenetv1():
     model = MobileNetV1()
-    return model.mobilev1_layers()
+    model.load_state_dict(torch.load('weights/mobilev1_withfc.pth')) # load pretrained model for detection
+    return model.mobilev1_layers() # exclude fc but keep weights
 
 # return a list of model features for mobileNet v2
 def mobilenetv2(width_mult = 1.):
     model = MobileNetV2(width_mult) # n_class and input_size is not used in backbone
-    return model.mobilev2_layers()
+    model.load_state_dict(torch.load('weights/mobilev2_withfc.pth')) # load pretrained model for detection, only pretrained for width_multi = 1
+    return model.mobilev2_layers() # exclude fc but keep weights
